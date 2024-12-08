5d5bcd89eae19077fe3bda24286530e3005fc646
karkisav <saurav.karki2@icloud.com>
2024-12-08 17:47:52 +0530

    updating the testing params

--- tests/api_endpoints.py
+++ tests/api_endpoints.py
@@ -13,17 +13,17 @@ class TestAPIEndpoints(unittest.TestCase):
         data = response.get_json()
         self.assertIn('recommendations', data)
 
-    # def test_feed_endpoint_with_username_and_category(self):
-    #     response = self.app.get('/feed?username=testuser&category_id=1')
-    #     self.assertEqual(response.status_code, 200)
-    #     data = response.get_json()
-    #     self.assertIn('recommendations', data)
+    def test_feed_endpoint_with_username_and_category(self):
+        response = self.app.get('/feed?username=doey&category_id=1')
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        self.assertIn('recommendations', data)
 
-    # def test_feed_endpoint_missing_username(self):
-    #     response = self.app.get('/feed')
-    #     self.assertEqual(response.status_code, 400)
-    #     data = response.get_json()
-    #     self.assertIn('error', data)
+    def test_feed_endpoint_missing_username(self):
+        response = self.app.get('/feed')
+        self.assertEqual(response.status_code, 400)
+        data = response.get_json()
+        self.assertIn('error', data)
 
 if __name__ == '__main__':
     unittest.main()
